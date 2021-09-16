89a7599caaeb889910f8fdc1d4c333307c2c8668
Shuhei Iriyama <67177058+shu-illy@users.noreply.github.com>
2021-09-16 17:47:59 +0900

    日毎の天気を表示

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -74,6 +74,56 @@ class _TopPageState extends State<TopPage> {
     ),
   ];
 
+  List<Weather> dailyWeather = [
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 1)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 3)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+    Weather(
+        temperatureMax: 20,
+        temperatureMin: 15,
+        rainyPercent: 0,
+        time: DateTime(2021, 10, 2)),
+  ];
+
+  List<String> weekDay = ['月', '火', '水', '木', '金', '土', '日'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -123,6 +173,52 @@ class _TopPageState extends State<TopPage> {
               ),
             ),
             Divider(height: 0),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    children: dailyWeather.map((weather) {
+                      return Container(
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                                width: 100,
+                                child: Text(
+                                    '${weekDay[dailyWeather[0].time.weekday - 1]}曜日')),
+                            Row(
+                              children: [
+                                Icon(Icons.wb_sunny_sharp),
+                                Text('${dailyWeather[0].rainyPercent}%',
+                                    style: TextStyle(color: Colors.blueAccent)),
+                              ],
+                            ),
+                            Container(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('${dailyWeather[0].temperatureMax}',
+                                      style: TextStyle(fontSize: 16)),
+                                  Text('${dailyWeather[0].temperatureMin}',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color:
+                                              Colors.black.withOpacity(0.4))),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
